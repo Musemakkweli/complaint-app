9ec24b19cc6b0625f6e5e1bbed0abe86c7cf3780
Musemakkweli <musemakwelibelyse@gmail.com>
2025-12-31 09:40:27 +0200

    Resolve merge conflict in API URL

--- constants/api.ts
+++ constants/api.ts
@@ -1,3 +1,3 @@
-// const API_URL = (process.env.API_URL as string) || "http://192.168.1.81:8000";
 const API_URL = (process.env.API_URL as string) || "http://10.197.223.252:8000";
+
 export default API_URL;
